a5fcefe0f2116b2165ca5d5eae148094e0283277
Kai LI <kai_li@outlook.com>
2021-03-14 22:14:49 +0000

    support vtable

--- my_vtable.h
+++ my_vtable.h
@@ -5,53 +5,80 @@ namespace my {
 
 class Animal {
 
-  public:
-    template <typename T>
-    Animal(const T& t)
-    : d_derived{static_cast<void*>(new T{t})}
-    , d_vtable{&Animal::s_table<T>}
-    {}
+public:
+  template <typename T>
+  Animal(const T &t)
+  : d_derived{new T{t}}
+  , d_vtable{&Animal::s_table<T>} {}
 
-    void speak() const {
-        d_vtable->speak(d_derived);
-    }
+  Animal(const Animal &animal)
+  : d_vtable{animal.d_vtable}
+  , d_derived{animal.d_vtable->copy(animal.d_derived)} {}
 
-    std::string toString() const {
-        return d_vtable->toString(d_derived);
-    }
+  Animal(Animal &&animal)
+  : d_vtable{animal.d_vtable}
+  , d_derived{animal.d_vtable->move(animal.d_derived)} {}
 
-    ~Animal() {
-        d_vtable->destroy(d_derived);
-        std::cout << "destroy animal instance" << std::endl;
-    }
+  void speak() const { d_vtable->speak(d_derived); }
 
-  private:
-    struct VTable {
-        void(*speak)(void*);
-        std::string(*toString)(void*);
-        void(*destroy)(void*);
-    };
+  std::string toString() const { return d_vtable->toString(d_derived); }
 
-    template <typename Derived>
-    static constexpr VTable s_table {
-        [](void* ptr) { static_cast<Derived*>(ptr)->speak(); },
-        [](void* ptr) { return static_cast<Derived*>(ptr)->toString(); },
-        [](void* ptr) { delete static_cast<Derived*>(ptr); }
-    };
+  ~Animal() {
+    d_vtable->destroy(d_derived);
+    std::cout << "destroy animal instance" << std::endl;
+  }
 
-    void*   d_derived;
-    const VTable* d_vtable;
+private:
+  struct VTable {
+    void (*speak)(void *);
+    std::string (*toString)(void *);
+    void *(*copy)(void *);
+    void *(*move)(void *);
+    void (*destroy)(void *);
+  };
+
+  template <typename Derived>
+  static constexpr VTable s_table{
+      [](void *ptr) { static_cast<Derived *>(ptr)->speak(); },
+      [](void *ptr) { return static_cast<Derived *>(ptr)->toString(); },
+      [](void *ptr) -> void * {
+        return new Derived(*(static_cast<Derived *>(ptr)));
+      },
+      [](void *ptr) -> void * {
+        auto origin = static_cast<Derived *>(ptr);
+        return new Derived(std::move(*origin));
+      },
+      [](void *ptr) { delete static_cast<Derived *>(ptr); }};
+
+  void *d_derived;
+  const VTable *d_vtable;
 };
-
 
 class Cat {
-  public:
-    ~Cat() {
-        std::cout << "destroy cat instance" << std::endl;
-    }
+public:
+  ~Cat() { std::cout << "destroy cat instance" << std::endl; }
 
-    void speak() const { std::cout << "Cat speaks" << std::endl; }
+  void speak() const { std::cout << "Cat speaks" << std::endl; }
 
-    std::string toString () const { return "Cat"; }
+  std::string toString() const { return "Cat"; }
 };
-}
+
+class Dog {
+public:
+  ~Dog() { std::cout << "destroy cat instance" << std::endl; }
+
+  void speak() const { std::cout << "Dog speaks" << std::endl; }
+
+  std::string toString() const { return "Dog"; }
+};
+
+class Rabbit {
+public:
+  ~Rabbit() { std::cout << "destroy rabbit instance" << std::endl; }
+
+  void speak() const { std::cout << "Rabbit speaks" << std::endl; }
+
+  std::string toString() const { return "Rabbit"; }
+};
+
+} // namespace my
--- test/my_vtable.t.cpp
+++ test/my_vtable.t.cpp
@@ -1,9 +1,20 @@
 #include "my_vtable.h"
+#include "vector"
 
 #include "gtest/gtest.h"
 
 TEST(my_vtable, tests) {
-    my::Animal animal = my::Cat();
+  using namespace my;
+  {
+    Animal animal = Cat{};
     animal.speak();
+  }
+  {
+    Animal animal = Rabbit{};
+    animal.speak();
+  }
+  std::vector<Animal> animals{Cat{}, Dog{}, Rabbit{}};
+  EXPECT_EQ(animals[0].toString(), "Cat");
+  EXPECT_EQ(animals[1].toString(), "Dog");
+  EXPECT_EQ(animals[2].toString(), "Rabbit");
 }
-
